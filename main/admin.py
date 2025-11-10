de9b1e8047d2367a0f0d64cee23e089932b7ef04
Your Name <you@example.com>
2025-11-10 21:44:08 +0500

    WIP

--- main/admin.py
+++ main/admin.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UzWord, OT, SIFAT
+from .models import UzWord, OT, SIFAT, UmumiyTurkum
 
 admin.site.register(UzWord)
 
-@admin.register(OT)
-class OTadmin(admin.ModelAdmin):
-    list_display = ['id','word',]
+@admin.register(UmumiyTurkum)
+class UmumiyTurkumadmin(admin.ModelAdmin):
+    list_display = ['id','word','type_is']
     search_fields = ['word']
-
-@admin.register(SIFAT)
-class SIFATadmin(admin.ModelAdmin):
-    list_display = ['id','word',]
-    search_fields = ['word']
+    list_filter = ['type_is',]
